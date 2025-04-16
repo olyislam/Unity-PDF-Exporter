d4979a4bab3ac29c95ae192f71af17fd1d7ce01d
olyislam <116255813+oly9@users.noreply.github.com>
2025-04-16 14:37:42 +0600

    Allow add image, bold & italic text

--- Assets/Scprits/PDFExporter.cs
+++ Assets/Scprits/PDFExporter.cs
@@ -1,8 +1,8 @@
 using System.IO;
+using UnityEngine;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using Org.BouncyCastle.Asn1.X509;
-using UnityEngine;
+using Font = iTextSharp.text.Font;
 using Document = iTextSharp.text.Document;
 
 public class PDFExporter : MonoBehaviour
@@ -15,8 +15,14 @@ public class PDFExporter : MonoBehaviour
     [Tooltip("The directory where you want to export the pdf document.")]
     public string filePath;
 
-    [Tooltip("The content that you want to save in the PDF document")]
-    public string content = "Hello from Unity!";
+    [Tooltip("The header that you want to save in the PDF document")]
+    public string header = "Header!";
+
+    [Tooltip("The modelName that you want to save in the PDF document")]
+    public string modelName = "Model Name!";
+
+    [Tooltip("Assign Image from Inspector")]
+    public Texture2D modelImage;
 
     void Start()
     {
@@ -36,17 +42,56 @@ public class PDFExporter : MonoBehaviour
         }
 
         string path = directory + docName + FILE_EXTENTION;
-        CreatePDF(path, content);
+        CreateFormattedPDF(path);
         Debug.Log("PDF created at: " + path);
    
     }
 
-    void CreatePDF(string filePath, string content)
+
+    void CreateFormattedPDF(string path)
     {
         Document document = new Document();
-        PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+        PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
         document.Open();
-        document.Add(new Paragraph(content));
+
+        BaseFont baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        Font headerFont = new Font(baseFont, 25, Font.BOLD);
+        Font italicFont = new Font(baseFont, 14, Font.ITALIC);
+
+
+        Paragraph header = new Paragraph(this.header, headerFont);
+        document.Add(header);
+
+        document.Add(new Paragraph(" "));
+        document.Add(new Paragraph(" "));
+
+        Paragraph someName = new Paragraph(modelName, italicFont);
+        document.Add(someName);
+
+        document.Add(new Paragraph(" "));
+
+        Paragraph imageLabel = new Paragraph("Model Image:", new Font(baseFont, 14));
+        document.Add(imageLabel);
+
+        if (modelImage != null)
+        {
+            Texture2D readableTex = new Texture2D(modelImage.width, modelImage.height, TextureFormat.RGBA32, false);
+            readableTex.SetPixels(modelImage.GetPixels());
+            readableTex.Apply();
+
+            byte[] imgBytes = readableTex.EncodeToPNG();
+            iTextSharp.text.Image pdfImg = iTextSharp.text.Image.GetInstance(imgBytes);
+
+            pdfImg.ScaleToFit(400f, 400f);
+            pdfImg.SpacingBefore = 10f;
+            pdfImg.Alignment = Element.ALIGN_LEFT;
+            document.Add(pdfImg);
+        }
+        else
+        {
+            Debug.LogWarning("Model Image not assigned. Please assign a Texture2D in the Inspector.");
+        }
+
         document.Close();
     }
 }
